a410b5f1d92960ffd6f3ff2a478f5e0eb486cfd1
Eric C Schoenborn <cultureondemand@gmail.com>
2017-08-28 11:19:06 -0400

    Update content.js

--- assets/js/content.js
+++ assets/js/content.js
@@ -55,24 +55,10 @@ Yet it also shows that vets are rising. Twenty-four percent of our members are p
       large: "1-Cover-large.jpg"
     },
     body: `
-<h3 class='prog'>DIVERSITY, ACCESS, AND INCLUSIVITY</h3>
-The program fosters an inclusive environment in which each participant’s individuality will be respected, understood, and embraced regardless of socioeconomic status, culture, ethnicity, religion, sex, age, national origin, medical conditions, physical ability, sexual orientation, and gender identity and/or expression.<br />
-<br />
-<h3 class='prog'>SAFE AND SUPPORTIVE ENVIRONMENT</h3>
-The program provides a safe and nurturing environment that supports the developmental, social-emotional, physical, and personal well -being of all children and youth.<br />
-<br />
-<h3 class='prog'>ACTIVE AND ENGAGED LEARNING</h3>
-The program design and activities utilize meaningful and engaging learning methods that promote collaboration and expand opportunities for children and youth.<br />
-<br />
-<h3 class='prog'>SKILL BUILDING</h3>
-The program maintains high expectations for all children and youth, intentionally links program learning goals with state and other standards, and provides activities to help participants achieve mastery.<br />
-<br />
-<h3 class='prog'>YOUTH VOICE AND LEADERSHIP</h3>
-The program provides and supports opportunities for children and youth to play a meaningful role in program design and implementation, and provides ongoing access to authentic leadership roles.<br />
-<br />
-<h3 class='prog'>HEALTHY CHOICES AND BEHAVIORS</h3>
-The program promotes the well-being of youth through opportunities to learn about and practice balanced nutrition, physical activity, and other healthy choices in an environment that supports and models a healthy lifestyle.
-    `
+ 
+<svg id="svg0"></svg>
+<svg id="svg1"></svg>
+<svg id="svg2"></svg>    `
   },
   
 
