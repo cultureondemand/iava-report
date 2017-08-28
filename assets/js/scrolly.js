bac88e239d183b9666baf5df40136b0130209aed
Eric C Schoenborn <cultureondemand@gmail.com>
2017-08-28 15:17:14 -0400

    Update scrolly.js

--- assets/js/scrolly.js
+++ assets/js/scrolly.js
@@ -210,6 +210,17 @@ var scrollVis = function() {
   function showTitle2() {
     
       
+   
+
+    
+  //age  
+drawStackChart(this.data_a);
+//gender
+drawStackChart(this.data_b);
+
+//gender
+drawStackChart(this.data_c);
+
  
   
   }
